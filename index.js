94cc1125c2e9b52758a9e186d962eeb0d0b50c52
4m12-code <112677210+4m12-code@users.noreply.github.com>
2023-11-19 13:25:24 -0500

    Iniciando proyecto loginApi

---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+● Se requiere realizar un servicio web para un registro y un inicio de sesión. El servicio recibirá un usuario y
+  una contraseña, si la autenticación es correcta saldrá un mensaje de autenticación satisfactoria en caso
+  contrario debe devolver error en la autenticación.
+● El código debe contener comentarios
+● Se debe crear el proyecto utilizando herramientas de versionamiento.
+*/
